73974bc4da320d5999a636c93f37a1a49a1ef5a6
Charlie Pryor <cep21@me.com>
2014-12-18 20:42:14 -0500

    commit 2
    commit 2 finish second method

---
+++ Pryor_Charles_assigment3 2.js
@@ -0,0 +1,53 @@
+//Charlie Pryor
+//12/18/14
+
+isEmailAddress("cep21@me.org");
+
+
+
+function isEmailAddress (a){
+	var result = false;
+	var sections = a.split('@');
+	
+	if (sections.length == 2) {
+		console.log("try1");
+		if (sections[1].indexOf(".com") > -1) {
+			console.log("contains .com");
+		
+		}
+		
+	}
+
+
+
+	return result;
+}
+
+
+
+
+
+
+function isPhoneNumber (b){
+	var result = false;
+	
+	var sections = b.split('-');
+	
+	if(sections.length == 3) {
+	
+		console.log(sections[0]);
+		//sections[0]
+		var num1 = parseInt(sections[0]);
+		var num2 = parseInt(sections[1]);
+		var num3 = parseInt(sections[2]);
+		if (num1 && num2 && num3) {
+		
+			if (sections[2].length == 4) {
+				console.log("last is 4 digits");
+				result = true;
+				//is number everything is a number no characters and last is 4 in length
+			}
+		}
+	}
+	return result;
+}
